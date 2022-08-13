b3aa657aa938402b2fe8888c90d983c800c3d5c9
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-13 17:09:09 +0700

    136. Adding Individual Meal Items & Displaying Them

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
+import MealItem from "./MeaItem/MealItem";
 
 AvailableMeals.propTypes = {};
 
@@ -32,11 +34,18 @@ const DUMMY_MEALS = [
 
 function AvailableMeals(props) {
   const mealsList = DUMMY_MEALS.map((meal) => (
-    <li key={meal.id}>{meal.name}</li>
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
   ));
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 }
---
+++ src/components/UI/Card.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./Card.module.css";
+
+Card.propTypes = {};
+
+function Card(props) {
+  return <div className={classes.card}>{props.children}</div>;
+}
+
+export default Card;
